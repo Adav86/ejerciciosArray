f5ec4db1b92dca3ff87f2a2fbe622d57d19bec08
Monica Alejandra Gallardo <adav020986@gmail.com>
2023-03-21 22:08:40 -0300

    guardado

--- ejerciciosArrays.js
+++ ejerciciosArrays.js
@@ -3,10 +3,21 @@
 /* 1)Declarar un array que vamos a llamar “clasificaciones” con los siguientes valores:
 Marcos, Franco, Agostina, Leon, Juan Cruz,Eduardo */
 
+let clasificaciones = ["Marcos", "Franco", "Agostina", "Leon", "Juan Cruz","Eduardo"]
+
 /* (vamos a suponer que esa es la orden de clasificación de un concurso) */
 
 /* 2)recorre el array (clasificaciones) e Imprime la clasificación actual.
  (realizar una funcion "mostrarClasificacion()" esa funcion debe recorrer el array con un bucle for y mostrar la clasificacion) */
+
+function mostrarClasificacion() {
+   /* for(let i=0; i<=clasificaciones.length-1; i ++ ){
+console.log(clasificaciones[i])
+    };*/
+    clasificaciones.forEach( i=> console.log(i));
+}
+mostrarClasificacion()
+
 
 //opcional ForEach (ojo tambien document.write)
 
